ed011ae15c5dd757165af94fbc75237b66e53bb3
vl.shagaliev <vl.shagaliev@yandex.ru>
2026-03-29 12:18:24 +0500

    Реализацию ФЗ 22

---
+++ commerce/payment/src/main/java/ru/yandex/practicum/model/Payment.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.UuidGenerator;
+import ru.yandex.practicum.dto.PaymentState;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @UuidGenerator
+    private UUID paymentId;
+
+    private UUID orderId;
+
+    private BigDecimal totalPayment;
+
+    private double deliveryTotal;
+
+    private double feeTotal;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState paymentState;
+
+}
